4d60ae123d59548cbe74e2c64c48c07808e0a13b
naren <blazeraze.naren@gmail.com>
2018-04-08 17:26:42 +0300

    refactored server.js for upload

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
         cb(null, './public/uploads');
     },
     filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+        cb(null, file.fieldname + moment().format('YYYYMMDDHHmmss') + path.extname(file.originalname));
     },
 });
 
@@ -64,7 +64,7 @@ const resize = (input, output, w, h) => {
 /* getting coordinate from image */
 const getCoordinates = (pathToImage) => {
     return new Promise((resolve, reject) => {
-        new ExifImage({ image: pathToImage }, (error, exifData) => {
+        new ExifImage({image: pathToImage}, (error, exifData) => {
             if (error) {
                 reject('Error: ' + error.message);
             } else {
@@ -128,52 +128,75 @@ app.get('/cat', (req, res) => {
     }).catch((err) => console.log(err));
 });
 
-app.post('/upload', (req, res) => {
+app.post('/upload', (req, res, next) => {
     upload(req, res, (err) => {
         if (err) {
             console.log('you fucked up' + err);
             res.sendStatus(400);
         } else {
-            // console.log(req.file);
-            // getCoordinates(__dirname + '/public/uploads/' + req.file.filename);
-            // console.log(lat, lng);
-
-            /* thumbnail */
-            resize(`./public/uploads/${req.file.filename}`, `./public/uploads/thumbnails/${req.file.filename}`, 320, 320).then((res) => {
-                console.log(res);
-            });
-            /* normal image */
-            resize(`./public/uploads/${req.file.filename}`, `./public/uploads/image/${req.file.filename}`, 770, 720).then((res) => {
-                console.log(res);
-            });
-            const smallCat = new Cat({
-                title: req.body.title,
-                category: req.body.category,
-                details: req.body.details,
-            });
-            smallCat['thumbnail'] = ` ${baseURL}/uploads/thumbnails/${req.file.filename}`;
-            smallCat['image'] = ` ${baseURL}/uploads/image/${req.file.filename}`;
-            smallCat['original'] = baseURL + '/uploads/' + req.file.filename;
-            smallCat['time'] = moment().format('YYYY-MM-DD HH:mm');
-            // console.log(smallCat);
-
-            /* post to db */
-            // Cat.create(smallCat).then((obj) => {
-            //     console.log('posted to db');
-            // }).catch((err) => console.log(err));
-            // res.redirect('/');
-
-            /* post to data1.json */
-            jsonfile.readFile(file, (err, obj) => {
-                let data = obj;
-                data.push(smallCat);
-                jsonfile.writeFile(file, data, {spaces: 4}, (err) => {
-                    console.log(err);
-                });
-            });
+            req.body['original'] = 'uploads/' + req.file.filename;
+            req.body['time'] = moment().format('YYYY-MM-DD HH:mm');
             console.log('successful file upload');
-            res.send('uploaded');
+            next();
         }
+    });
+});
+
+// get coordinates from EXIF
+app.use('/upload', (req, res, next) => {
+    getCoordinates(req.file.path).then((coords) => {
+        req.body.coordinates = coords;
+        console.log('exif working');
+        next();
+    });
+});
+
+// make small thumbnail
+app.use('/upload', (req, res, next) => {
+    const thumbPath = '/uploads/thumbnails/' + req.file.filename;
+    resize(req.file.path, 'public' + thumbPath, 320, 320).
+        then((res) => {
+            console.log('image to thumbnail');
+            req.body.thumbnail = thumbPath;
+            next();
+        });
+});
+
+// make medium thumbnail
+app.use('/upload', (req, res, next) => {
+    const medPath = '/uploads/image/' + req.file.filename;
+    resize(req.file.path, 'public' + medPath, 770, 720).
+        then((res) => {
+            console.log('normal sized');
+            req.body.image = medPath;
+            next();
+        });
+});
+
+// app.use('/upload', (req, res) => {
+//     /* post to data1.json */
+//     jsonfile.readFile(file, (err, obj) => {
+//         let data = obj;
+//         data.push(req.body);
+//         jsonfile.writeFile(file, data, {spaces: 4}, (err) => {
+//             console.log(err);
+//         });
+//     });
+//     res.send('posted to json');
+// });
+
+app.use('/upload', (req, res) => {
+    Cat.create(req.body).then((post) => {
+        res.send(post);
+    });
+});
+
+app.get('/:id', (req, res) => {
+    Cat.findById(req.params.id).then((err, data) => {
+        if (err) res.send(err);
+        else {
+            res.send('cat fetched with id\n' + data)
+        };
     });
 });
 
